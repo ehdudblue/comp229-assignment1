c526e8ff3c1ccd4517a8e928318c831d046df6c8
Do Young Lee <ehdudblue@gmail.com>
2022-10-05 02:29:37 -0400

    final version

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -1,3 +1,4 @@
+// Assignment1 / Do Young Lee / 301108844 / 2022-10-05
 // IIFE -- Immediately Invoked Function Expression
 (function(){
 
@@ -9,3 +10,12 @@
     window.addEventListener("load", start);
 
 })();
+
+// It directs users to the homepage when the "send button" is clicked
+(function() {
+    function send() {
+        window.location.href = "http://localhost:3000/";
+    }
+    document.getElementById('send').addEventListener("click", send);
+})();
+
